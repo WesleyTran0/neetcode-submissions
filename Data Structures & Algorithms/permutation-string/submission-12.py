3dd2162273b5a67920a8c25b75254cbd77147830
Wesley Tran <wesley.tran24@gmail.com>
2026-08-21 17:16:11 -0400

    Add: permutation-string - submission-12

---
+++ Data Structures & Algorithms/permutation-string/submission-12.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        mp = {}
+        for i in s1:
+            mp[i] = 1 + mp.get(i, 0)
+        
+        print(mp)
+        wndw = {}
+        l = 0
+        for r in range(len(s2)):
+            if r < len(s1):
+                wndw[s2[r]] = 1 + wndw.get(s2[r], 0)
+                continue
+            
+            if wndw.items() == mp.items():
+                return True
+                
+            wndw[s2[r]] = 1 + wndw.get(s2[r], 0)
+            wndw[s2[l]] -= 1
+            if wndw[s2[l]] == 0:
+                del wndw[s2[l]]
+            l += 1
+            print(wndw)
+        if wndw.items() == mp.items():
+            return True
+            
+        return False
